9934ebac2d708a95d396e8c9c7aba4880234e4b1
Effortless <30777722+effortlessdevsec@users.noreply.github.com>
2021-02-13 14:40:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ echo "downloading jadx"
 git clone https://github.com/skylot/jadx.git ; cd jadx ; ./gradlew dist
 sudo cp build/jadx/bin/jadx /usr/loca/bin
 
+go get -u github.com/tomnomnom/gf 
+echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
