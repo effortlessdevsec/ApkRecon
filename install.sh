35569a01ae29bd1642d4fc038377749bdf5246ca
Effortless <30777722+effortlessdevsec@users.noreply.github.com>
2021-02-13 15:17:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ echo "downloading jadx"
 git clone https://github.com/skylot/jadx.git ; cd jadx ; ./gradlew dist
 sudo cp build/jadx/bin/jadx /usr/loca/bin
 
-go get -u github.com/tomnomnom/gf 
+go get -u github.com/tomnomnom/gf
+
+echo "make sure you have set the GOPATH"
 echo 'source $GOPATH/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
 cp -r $GOPATH/src/github.com/tomnomnom/gf/examples ~/.gf
 cp config/Apksecret.json ~/.gf
